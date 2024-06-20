0226871c7ac00bc1750e592adbb7b2fa78ca23f2
PaulChass <paulchasseuil@hotmail.fr>
2024-06-20 21:12:47 +0200

    modifications to make subfolders display seperatly

--- backend/routes/folder.js
+++ backend/routes/folder.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { getFolders, createFolder, uploadFiles  } = require('../controllers/folderController');
+const { getFolders, createFolder, uploadFiles } = require('../controllers/folderController');
 const router = express.Router();
 const  authenticate  = require('../middleware/auth');
 
 
 // Fetch folder structure
-router.get('/', authenticate , getFolders);
+router.get('/:parent_id?', authenticate , getFolders);
+
 
 // Create a new folder
 router.post('/', authenticate , createFolder);
